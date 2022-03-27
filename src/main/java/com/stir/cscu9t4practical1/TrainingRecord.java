fb5809c55d4d0f624e16019412e81731adfdee86
KirinKai <98621278+KirinKai@users.noreply.github.com>
2022-03-28 00:38:10 +0100

    Add files via upload

--- src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
+++ src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
@@ -2,8 +2,6 @@
 package com.stir.cscu9t4practical1;
 
 
-
-
 import java.util.*;
 
 
@@ -22,7 +20,7 @@ public class TrainingRecord {
    // look up the entry of a given day and month
    public String lookupEntry (int d, int m, int y) {
        ListIterator<Entry> iter = tr.listIterator();
-       String result = "No entries found";
+       String result = "No Entries Found";
        while (iter.hasNext()) {
           Entry current = iter.next();
           if (current.getDay()==d && current.getMonth()==m && current.getYear()==y) 
@@ -30,6 +28,21 @@ public class TrainingRecord {
             }
        return result;
    } // lookupEntry
+   
+   // find all the entries of a given day and month
+   public String findallEntry (int d, int m, int y) {
+       ListIterator<Entry> iter = tr.listIterator();
+       String result = "";
+       while (iter.hasNext()) {
+          Entry current = iter.next();
+          if (current.getDay()==d && current.getMonth()==m && current.getYear()==y) 
+             result += current.getEntry();
+            }
+       
+       if (result == "") result = "No Entries Found";
+       return result;
+   } // findallEntry
+   
    
    // Count the number of entries
    public int getNumberOfEntries(){
@@ -39,5 +52,35 @@ public class TrainingRecord {
    public void clearAllEntries(){
        tr.clear();
    }
+
+   public String uniqueValidate(String n, int m, int d, int y) {
+	   String message = "";
+	   ListIterator<Entry> iter = tr.listIterator();
+	   
+	   while (iter.hasNext()) {
+		   Entry current = iter.next();
+		   if (current.getName().equalsIgnoreCase(n) && current.getDay()==d && current.getMonth()==m && current.getYear()==y)
+			   message = "Error, This Entry already exists";
+		   }
+	   
+	   return message;
+}
+
+   public String removeEntry(String n, int d, int m, int y) {
+	   ListIterator<Entry> iter = tr.listIterator();
+       String result = "Specified Entry not found.";
+       int indexRemove = -1;
+       while (iter.hasNext()) {
+    	   Entry current = iter.next();
+    	   if (current.getName().equalsIgnoreCase(n) && current.getDay()==d && current.getMonth()==m && current.getYear()==y)
+    		   
+    		   indexRemove = tr.indexOf(current);
+            }
+       if(indexRemove != -1) {
+    	   tr.remove(indexRemove);
+    	   result = "Entry Removed.";
+       }
+	   return result;
+}
    
 } // TrainingRecord
--- src/main/java/com/stir/cscu9t4practical1/TrainingRecordGUI.java
+++ src/main/java/com/stir/cscu9t4practical1/TrainingRecordGUI.java
@@ -9,6 +9,8 @@ import java.lang.Number;
 
 public class TrainingRecordGUI extends JFrame implements ActionListener {
 
+	private String[] Entries = {"Sprint", "Swim", "Cycle"};
+	
     private JTextField name = new JTextField(30);
     private JTextField day = new JTextField(2);
     private JTextField month = new JTextField(2);
@@ -17,6 +19,9 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
     private JTextField mins = new JTextField(2);
     private JTextField secs = new JTextField(2);
     private JTextField dist = new JTextField(4);
+    private JTextField add1 = new JTextField(6);
+    private JTextField add2 = new JTextField(6);
+    private JComboBox<String> entry = new JComboBox<>(Entries);
     private JLabel labn = new JLabel(" Name:");
     private JLabel labd = new JLabel(" Day:");
     private JLabel labm = new JLabel(" Month:");
@@ -24,9 +29,15 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
     private JLabel labh = new JLabel(" Hours:");
     private JLabel labmm = new JLabel(" Mins:");
     private JLabel labs = new JLabel(" Secs:");
+    private JLabel labe = new JLabel(" Entry Type:");
+    private JLabel labad1 = new JLabel(" Quantity:");
+    private JLabel labad2 = new JLabel(" Recovery:");
     private JLabel labdist = new JLabel(" Distance (km):");
     private JButton addR = new JButton("Add");
     private JButton lookUpByDate = new JButton("Look Up");
+    private JButton findAllByDate = new JButton("Find All");
+    private JButton remove = new JButton("Remove");
+    
 
     private TrainingRecord myAthletes = new TrainingRecord();
 
@@ -64,10 +75,25 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
         add(labdist);
         add(dist);
         dist.setEditable(true);
+        add(labe);
+        add(entry);
+        entry.addActionListener(this);
+        add(labad1);
+        add(add1);
+        add1.setEditable(true);
+        add(labad2);
+        add(add2);
+        add2.setEditable(true);
+        
         add(addR);
         addR.addActionListener(this);
         add(lookUpByDate);
         lookUpByDate.addActionListener(this);
+        add(findAllByDate);
+        findAllByDate.addActionListener(this);
+        add(remove);
+        remove.addActionListener(this);
+        
         add(outputArea);
         outputArea.setEditable(false);
         setSize(720, 200);
@@ -83,13 +109,44 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent event) {
         String message = "";
         if (event.getSource() == addR) {
-            message = addEntry("generic");
+            if (entry.getSelectedItem() == "Sprint") {
+            	message = addSprintEntry();
+            }
+            if (entry.getSelectedItem() == "Swim") {
+            	message = addSwimEntry();
+            }
+            if (entry.getSelectedItem() == "Cycle") {
+            	message = addCycleEntry();
+            }
+            blankDisplay();
         }
         if (event.getSource() == lookUpByDate) {
             message = lookupEntry();
+            blankDisplay();
+        }
+        if (event.getSource() == findAllByDate) {
+            message = findallEntry();
+            blankDisplay();
+        }
+        if (event.getSource() == remove) {
+        	message = removeEntry();
+        	blankDisplay();
+        }
+        if (event.getSource() == entry) {
+        	if (entry.getSelectedItem() == "Sprint") {
+        			labad1.setText(" Quantity:");
+        			labad2.setText(" Recovery:");
+        	}
+        	if (entry.getSelectedItem() == "Swim") {
+    			labad1.setText(" Locale:");
+    			labad2.setText(" N/A:");
+        	}
+        	if (entry.getSelectedItem() == "Cycle") {
+    			labad1.setText(" Terrain:");
+    			labad2.setText(" Tempo:");
+        	}
         }
         outputArea.setText(message);
-        blankDisplay();
     } // actionPerformed
 
     public String addEntry(String what) {
@@ -108,13 +165,184 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
         return message;
     }
     
-    public String lookupEntry() {
+    public String addSprintEntry() {
+        String message = "Record added\n";
+        if(name.getText().isEmpty() ||
+        		month.getText().isEmpty() || 
+        		day.getText().isEmpty() || 
+        		year.getText().isEmpty() ||
+        		dist.getText().isEmpty() || 
+        		hours.getText().isEmpty() || 
+        		mins.getText().isEmpty() || 
+        		secs.getText().isEmpty() || 
+        		add1.getText().isEmpty() || 
+        		add2.getText().isEmpty()  
+        		) {
+        	message = "Error, Please fill all fields";
+        } else if(Integer.parseInt(day.getText()) < 1 || Integer.parseInt(day.getText()) > 31 ||
+        		Integer.parseInt(month.getText()) < 1 || Integer.parseInt(month.getText()) > 12 ||
+        		year.getText().length() != 4
+        		){
+        	message = "Error, Please enter a valid date";
+        } else {
+        System.out.println("Adding Sprint entry to the records");
+        String n = name.getText();
         int m = Integer.parseInt(month.getText());
         int d = Integer.parseInt(day.getText());
         int y = Integer.parseInt(year.getText());
-        outputArea.setText("looking up record ...");
-        String message = myAthletes.lookupEntry(d, m, y);
+        float km = java.lang.Float.parseFloat(dist.getText());
+        int h = Integer.parseInt(hours.getText());
+        int mm = Integer.parseInt(mins.getText());
+        int s = Integer.parseInt(secs.getText());
+        int q = Integer.parseInt(add1.getText());
+        int r = Integer.parseInt(add2.getText());
+        SprintEntry e = new SprintEntry(n, d, m, y, h, mm, s, km, q, r);
+        String unique = uniqueValidate(e);
+        if(unique == "") {
+        	myAthletes.addEntry(e);
+        } else {
+        	message = unique;
+        }}
+        
         return message;
+    }
+    
+    public String addSwimEntry() {
+        String message = "Record added\n";
+        if(name.getText().isEmpty() ||
+        		month.getText().isEmpty() || 
+        		day.getText().isEmpty() || 
+        		year.getText().isEmpty() ||
+        		dist.getText().isEmpty() || 
+        		hours.getText().isEmpty() || 
+        		mins.getText().isEmpty() || 
+        		secs.getText().isEmpty() || 
+        		add1.getText().isEmpty()
+        		) {
+        	message = "Error, Please fill all fields";
+        } else if(Integer.parseInt(day.getText()) < 1 || Integer.parseInt(day.getText()) > 31 ||
+        		Integer.parseInt(month.getText()) < 1 || Integer.parseInt(month.getText()) > 12 ||
+        		year.getText().length() != 4
+        		){
+        	message = "Error, Please enter a valid date";
+        } else {
+        System.out.println("Adding Swim entry to the records");
+        String n = name.getText();
+        int m = Integer.parseInt(month.getText());
+        int d = Integer.parseInt(day.getText());
+        int y = Integer.parseInt(year.getText());
+        float km = java.lang.Float.parseFloat(dist.getText());
+        int h = Integer.parseInt(hours.getText());
+        int mm = Integer.parseInt(mins.getText());
+        int s = Integer.parseInt(secs.getText());
+        String l = add1.getText();
+        SwimEntry e = new SwimEntry(n, d, m, y, h, mm, s, km, l);
+        String valid = swimValidate(e);
+        String unique = uniqueValidate(e);
+        if(valid == "" && unique == "") {
+        	myAthletes.addEntry(e);
+        } else {
+        	if(!unique.equalsIgnoreCase("")) {
+        		message = unique;
+        	} else {
+        		message = valid;
+        	}
+        }}
+        
+        return message;
+    }
+    
+    public String addCycleEntry() {
+        String message = "Record added\n";
+        if(name.getText().isEmpty() ||
+        		month.getText().isEmpty() || 
+        		day.getText().isEmpty() || 
+        		year.getText().isEmpty() ||
+        		dist.getText().isEmpty() || 
+        		hours.getText().isEmpty() || 
+        		mins.getText().isEmpty() || 
+        		secs.getText().isEmpty() || 
+        		add1.getText().isEmpty() || 
+        		add2.getText().isEmpty()  
+        		) {
+        	message = "Error, Please fill all fields";
+        } else if(Integer.parseInt(day.getText()) < 1 || Integer.parseInt(day.getText()) > 31 ||
+        		Integer.parseInt(month.getText()) < 1 || Integer.parseInt(month.getText()) > 12 ||
+        		year.getText().length() != 4
+        		){
+        	message = "Error, Please enter a valid date";
+        } else {
+        
+        System.out.println("Adding Cycle entry to the records");
+        String n = name.getText();
+        int m = Integer.parseInt(month.getText());
+        int d = Integer.parseInt(day.getText());
+        int y = Integer.parseInt(year.getText());
+        float km = java.lang.Float.parseFloat(dist.getText());
+        int h = Integer.parseInt(hours.getText());
+        int mm = Integer.parseInt(mins.getText());
+        int s = Integer.parseInt(secs.getText());
+        String t = add1.getText();
+        String tt = add2.getText();
+        CycleEntry e = new CycleEntry(n, d, m, y, h, mm, s, km, t, tt);
+        String unique = uniqueValidate(e);
+        if(unique == "") {
+        	myAthletes.addEntry(e);
+        } else {
+        	message = unique;
+        }}
+        
+        return message;
+    }
+    
+    public String lookupEntry() {
+    	String message = "";
+        
+        if(month.getText().isEmpty() || day.getText().isEmpty() || year.getText().isEmpty()) {
+        	message = "Error, Please fill all Date fields";
+        } else {
+        	int m = Integer.parseInt(month.getText());
+            int d = Integer.parseInt(day.getText());
+            int y = Integer.parseInt(year.getText());
+        outputArea.setText("looking up record ...");
+        message = myAthletes.lookupEntry(d, m, y);
+        }
+        return message;
+    }
+    
+    public String findallEntry() {
+    	String message = "";
+    	
+        if(month.getText().isEmpty() || day.getText().isEmpty() || year.getText().isEmpty()) {
+        	message = "Error, Please fill all Date fields";
+        } else {
+        	int m = Integer.parseInt(month.getText());
+            int d = Integer.parseInt(day.getText());
+            int y = Integer.parseInt(year.getText());
+            
+        	outputArea.setText("looking up record ...");
+        	message = myAthletes.findallEntry(d, m, y);
+        }
+        return message;
+    }
+    
+    public String removeEntry() {
+    	
+    	String message = "";
+    	
+        if(name.getText().isEmpty() || month.getText().isEmpty() || day.getText().isEmpty() || year.getText().isEmpty()) {
+        	message = "Error, Please fill Name and all Date fields";
+        } else {
+        	String n = name.getText();
+        	int m = Integer.parseInt(month.getText());
+            int d = Integer.parseInt(day.getText());
+            int y = Integer.parseInt(year.getText());
+        	
+        	outputArea.setText("removing record ...");
+        message = myAthletes.removeEntry(n, d, m, y);
+        }
+        
+    	return message;
     }
 
     public void blankDisplay() {
@@ -126,6 +354,8 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
         mins.setText("");
         secs.setText("");
         dist.setText("");
+        add1.setText("");
+        add2.setText("");
 
     }// blankDisplay
     // Fills the input fields on the display for testing purposes only
@@ -140,5 +370,33 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
         dist.setText(String.valueOf(ent.getDistance()));
     }
 
+    
+    public String uniqueValidate(Entry e) {
+    	String unique = "";
+    	
+    	String n = e.getName();
+    	int m = e.getMonth();
+        int d = e.getDay();
+        int y = e.getYear();
+        
+        unique = myAthletes.uniqueValidate(n, m, d, y);
+        
+    	return unique;
+    }
+    
+    
+    public String swimValidate(SwimEntry e) {
+ 	   String valid = "";
+ 	   
+ 	   if(!e.getLocale().equalsIgnoreCase("outside") && !e.getLocale().equalsIgnoreCase("in a pool")) {
+ 		   valid = "Error, Locale not valid.";
+ 	   }  	   
+ 	   
+ 	   return valid;
+    }
+    
+    
+    
+    
 } // TrainingRecordGUI
 
